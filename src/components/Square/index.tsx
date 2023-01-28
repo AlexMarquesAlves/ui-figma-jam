a3d13b5d14a9de3be4cd084075c615b2702166ad
¡Duque <atanaelleonardo@gmail.com>
2023-01-28 01:23:59 -0300

    feat: added some styles on <Handle /> component

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -5,10 +5,30 @@ export const Square = (_props: NodeProps) => {
   return (
     <>
       <div className={'bg-violet-500 rounded w-[200px] h-[200px]'}>
-        <Handle id="top" type="source" position={Position.Top} />
-        <Handle id="right" type="source" position={Position.Right} />
-        <Handle id="bottom" type="source" position={Position.Bottom} />
-        <Handle id="left" type="source" position={Position.Left} />
+        <Handle
+          id="top"
+          type="source"
+          position={Position.Top}
+          className={'w-3 h-3 bg-blue-400/80 -top-5'}
+        />
+        <Handle
+          id="right"
+          type="source"
+          position={Position.Right}
+          className={'w-3 h-3 bg-blue-400/80 -right-5'}
+        />
+        <Handle
+          id="bottom"
+          type="source"
+          position={Position.Bottom}
+          className={'w-3 h-3 bg-blue-400/80 -bottom-5'}
+        />
+        <Handle
+          id="left"
+          type="source"
+          position={Position.Left}
+          className={'w-3 h-3 bg-blue-400/80 -left-5'}
+        />
       </div>
     </>
   );
