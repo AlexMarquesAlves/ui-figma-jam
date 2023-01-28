f1d2d37e9a7610824c81b521facd4c5e436bdb75
¡Duque <atanaelleonardo@gmail.com>
2023-01-28 01:47:43 -0300

    feat: created custom edge component

--- src/components/Edges/DefaultEdge/index.tsx
+++ src/components/Edges/DefaultEdge/index.tsx
@@ -1,7 +1,46 @@
-export const DefaultEdge = () => {
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { EdgeProps, getSmoothStepPath } from 'reactflow';
+
+export const DefaultEdge = ({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  style = {},
+  data,
+  markerEnd,
+}: EdgeProps) => {
+  const [edgePath] = getSmoothStepPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  });
+
   return (
     <>
-      <h1>DefaultEdge</h1>
+      <path
+        id={id}
+        style={style}
+        className="react-flow__edge-path"
+        d={edgePath}
+        markerEnd={markerEnd}
+      />
+      {/* <text>
+        <textPath
+          href={`#${id}`}
+          style={{ fontSize: 12 }}
+          startOffset="50%"
+          textAnchor="middle"
+        >
+          {data.text}
+        </textPath>
+      </text> */}
     </>
   );
 };
