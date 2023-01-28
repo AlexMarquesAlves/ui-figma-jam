cfe95f24d3454bbeb537adfee98c3a1e176a311f
¡Duque <atanaelleonardo@gmail.com>
2023-01-28 01:08:28 -0300

    feat: showing <Square /> component on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import ReactFlow, { Background, Controls } from 'reactflow';
+import ReactFlow, { Background, Controls, Node } from 'reactflow';
 import 'reactflow/dist/style.css';
 import { zinc } from 'tailwindcss/colors';
+import { Square } from './components/Square';
+
+const NODE_TYPES = {
+  square: Square,
+};
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 200,
+      y: 400,
+    },
+    data: {},
+  },
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 1000,
+      y: 400,
+    },
+    data: {},
+  },
+  // eslint-disable-next-line prettier/prettier
+]satisfies Node[];
 
 function App() {
   return (
     <div className={'w-screen h-screen'}>
-      <ReactFlow>
+      <ReactFlow nodeTypes={NODE_TYPES} nodes={INITIAL_NODES}>
         <Background gap={12} size={2} color={zinc[200]} />
         <Controls />
       </ReactFlow>
